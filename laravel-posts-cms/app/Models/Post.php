e9db13efb3a042cbe84c27f075b3505d2518e10a
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-04-17 22:32:32 +0900

    chore(post): set fillable, casta

--- laravel-posts-cms/app/Models/Post.php
+++ laravel-posts-cms/app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected array $fillable = [
+        'title',
+        'body',
+        'is_public',
+        'published_at',
+    ];
+
+    protected array $casts = [
+        'is_public' => 'bool',
+        'published_at' => 'datetime',
+    ];
 }
