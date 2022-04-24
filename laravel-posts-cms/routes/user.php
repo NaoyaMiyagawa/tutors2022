997d36b167a9e7e49ac7e5b2375f8caafdf2082e
NaoyaMiyagawa <tsuna2764@gmail.com>
2022-04-24 13:12:40 +0800

    refactor(route): rewrite with controller method

--- laravel-posts-cms/routes/user.php
+++ laravel-posts-cms/routes/user.php
@@ -1,4 +1,6 @@
 <?php
 
-Route::get('/', 'PostController@index')->name('home');
-Route::resource('posts', 'PostController')->only(['index', 'show']);
+Route::controller(PostController::class)->group(function () {
+    Route::get('/', 'index')->name('home');
+    Route::resource('posts', PostController::class)->only(['index', 'show']);
+});
